2410b09ab92cfe24b13463199b0f3c21ab6254bf
zion <zion@DESKTOP-HJ8OSMC.>
2024-10-01 19:15:40 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+//Variables that when used to multiply it gives 62
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+console.log(multiply); 
+//Variable on Math.random
+let random = Math.floor(Math.random() * 7); 
+console.log(random);
+//Variable on  Mod
+let num3 = 24;
+let num4 = 20;
+let mod = num3 % num4;
+console.log(mod);
+//Variable on Maximum Numbers
+let numbers = (0, 5, 10, 15, 20)
+let max = Math.max(0, 5, 10, 15, 20)
